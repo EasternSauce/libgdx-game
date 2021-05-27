ac8b684725394adc9e428b20202b0ca945d2767b
Kamil <kam.kurp@gmail.com>
2021-05-27 20:30:53 +0200

    border tiles

--- src/main/scala/com/easternsauce/libgdxgame/area/traits/PhysicalTerrain.scala
+++ src/main/scala/com/easternsauce/libgdxgame/area/traits/PhysicalTerrain.scala
@@ -9,7 +9,16 @@ import com.easternsauce.libgdxgame.area.AreaTile
 trait PhysicalTerrain extends TiledGrid {
   val world: World = new World(new Vector2(0, 0), true)
 
+  val firstLayer: TiledMapTileLayer =
+    map.getLayers.get(0).asInstanceOf[TiledMapTileLayer]
+
   def initPhysicalTerrain(): Unit = {
+    createMapTerrain()
+    createBorders()
+
+  }
+
+  private def createMapTerrain(): Unit = {
     for (layerNum <- 0 to 1) { // two layers
       val layer: TiledMapTileLayer =
         map.getLayers.get(layerNum).asInstanceOf[TiledMapTileLayer]
@@ -57,9 +66,68 @@ trait PhysicalTerrain extends TiledGrid {
             tiles += (layerNum, x, y) -> tile
 
           }
+
         }
 
       }
+
     }
+  }
+
+  private def createBorders(): Unit = {
+    for { x <- Seq.range(0, firstLayer.getWidth) } {
+
+      var rectX = x * firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      var rectY = (-1) * firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+      var rectW = firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      var rectH = firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+
+      createBorderTile(rectX, rectY, rectW, rectH)
+
+      rectX = x * firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      rectY = firstLayer.getHeight * firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+      rectW = firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      rectH = firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+
+      createBorderTile(rectX, rectY, rectW, rectH)
+    }
+
+    for { y <- Seq.range(0, firstLayer.getHeight) } {
+
+      var rectX = (-1) * firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      var rectY = y * firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+      var rectW = firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      var rectH = firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+
+      createBorderTile(rectX, rectY, rectW, rectH)
+
+      rectX = firstLayer.getWidth * firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      rectY = y * firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+      rectW = firstLayer.getTileWidth * mapScale / LibgdxGame.PPM
+      rectH = firstLayer.getTileHeight * mapScale / LibgdxGame.PPM
+
+      createBorderTile(rectX, rectY, rectW, rectH)
+    }
+  }
+
+  private def createBorderTile(rectX: Float, rectY: Float, rectW: Float, rectH: Float) = {
+    val bodyDef = new BodyDef()
+    bodyDef.`type` = BodyDef.BodyType.StaticBody
+    bodyDef.position
+      .set(rectX + rectH / 2, rectY + rectH / 2)
+
+    val body: Body = world.createBody(bodyDef)
+
+    body.setUserData(this)
+
+    val shape: PolygonShape = new PolygonShape()
+
+    shape.setAsBox(rectW / 2, rectH / 2)
+
+    val fixtureDef: FixtureDef = new FixtureDef
+
+    fixtureDef.shape = shape
+
+    body.createFixture(fixtureDef)
   }
 }
